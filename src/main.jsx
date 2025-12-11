c51646ffc1b71aa87ba2d1ca45818a4f87ede203
Wong Wai Keat <waikeat.wong90@gmail.com>
2025-12-11 22:09:50 +0900

    [Index css, Main] Add fonts into css and main jsx to improve on CLS

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import './i18n'
 
+// Fonts
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/open-sans/400.css';
+import '@fontsource/open-sans/500.css';
+import '@fontsource/open-sans/600.css';
+import '@fontsource/open-sans/700.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
